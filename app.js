015833a325d6c7042cdcaccd24505b3cac8a97a2
Dipesh <dpes.adikari44@gmail.com>
2024-07-05 15:22:12 +0545

    Initial Commit

---
+++ app.js
@@ -0,0 +1,120 @@
+var axios = require("axios");
+var dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+dotenv.config();
+
+var getEmailUrl = "https://api.ontraport.com/1/object/getByEmail";
+var getContactUrl = "https://api.ontraport.com/1/Contacts";
+
+const corsOptions = {
+  origin: "https://members.awardee.com.au",
+  // You can also configure other CORS options here if needed
+};
+app.use(cors(corsOptions));
+
+var headers = {
+  "Api-Appid": process.env.API_APPID,
+  "Api-Key": process.env.API_KEY,
+};
+
+var fetchContactId = function (email) {
+  var params = {
+    objectID: 0,
+    email: email,
+    all: 0,
+  };
+
+  var url = getEmailUrl + "?" + new URLSearchParams(params);
+
+  return axios(url, {
+    method: "GET",
+    headers: headers,
+  })
+    .then(function (response) {
+      if (response.status !== 200) {
+        throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+      }
+      return response.data;
+    })
+    .then(function (data) {
+      if (data && data.data && data.data.id) {
+        return data.data.id;
+      } else {
+        throw new Error("Contact ID not found");
+      }
+    })
+    .catch(function (error) {
+      throw new Error(`Error fetching contact ID: ${error.message}`);
+    });
+};
+
+var fetchContactDetailsById = function (contactId) {
+  var params = {
+    ids: contactId,
+  };
+
+  var url = getContactUrl + "?" + new URLSearchParams(params);
+
+  return axios(url, {
+    method: "GET",
+    headers: headers,
+  })
+    .then(function (response) {
+      if (response.status !== 200) {
+        throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+      }
+      return response.data;
+    })
+    .then(function (data) {
+      if (data && data.data && data.data.length > 0) {
+        var email = data.data[0].email;
+        var firstname = data.data[0].firstname;
+        var lastname = data.data[0].lastname;
+        return { email: email, firstname: firstname, lastname: lastname };
+      } else {
+        throw new Error("Contact details not found");
+      }
+    })
+    .catch(function (error) {
+      throw new Error(`Error fetching contact details: ${error.message}`);
+    });
+};
+
+var fetchContactDetailsByEmail = function (email) {
+  return fetchContactId(email)
+    .then(function (contactId) {
+      return fetchContactDetailsById(contactId);
+    })
+    .catch(function (error) {
+      throw new Error(`Error fetching contact details by email: ${error.message}`);
+    });
+};
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async function (req, res) {
+  let email = req.query.email;
+  if (!email) {
+    return res.status(400).json({ error: "Email is required" });
+  }
+  try {
+    const contactDetails = await fetchContactDetailsByEmail(email);
+    res.status(200).json(contactDetails);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
+
+app.listen("3002", function (err) {
+  if (err) {
+    console.error("Error starting server:", err);
+  } else {
+    console.log("Listening on port 3002");
+  }
+});
